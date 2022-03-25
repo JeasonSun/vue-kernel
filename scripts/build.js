66272634a62dff93a4af079dbfded5451c746a63
mojie <jeason8023@gmail.com>
2022-03-25 23:12:04 +0800

    feat: rollup

---
+++ scripts/build.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const execa = require("execa");
+
+const targets = fs
+  .readdirSync("packages")
+  .filter((f) => fs.statSync(`packages/${f}`).isDirectory());
+
+async function build(target) {
+  await execa("rollup", ["-c", "--environment", `TARGET:${target}`], {
+    stdio: "inherit",
+  });
+}
+
+function runParallel(targets, iteratorFn) {
+  const res = [];
+
+  for (const item of targets) {
+    const p = iteratorFn(item);
+    res.push(p);
+  }
+  return Promise.all(res);
+}
+
+runParallel(targets, build);
